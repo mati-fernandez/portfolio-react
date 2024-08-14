3b584b902205b2e67619fdf5d76db339a5ea031b
Matias Fernandez <matifer.dev@gmail.com>
2024-08-13 22:17:12 -0300

    First modal is operative; useModal hook added!

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,18 @@
 import { useContext } from 'react';
 import { TranslationContext } from '../i18n/TranslationContext';
 import imgrqm from '../assets/random-quote-machine.png';
+import { useModal } from '../hooks/useModal';
 
 const Projects = () => {
   const { translate } = useContext(TranslationContext);
+  const [isOpen1, openModal1, closeModal1] = useModal();
 
   return (
     <div className="page menu">
-      <a
-        className="long-text button link"
-        href="https://mati-fernandez.github.io/random-quote-machine/"
-        target="_blank"
-      >
+      <a className="long-text button link" target="_blank" onClick={openModal1}>
         {translate('projects.item1.title')}
       </a>
-      <article className="modal is-open">
+      <article className={`modal ${isOpen1 && 'is-open'}`}>
         <div className="modal-container detail">
           <h3>{translate('projects.item1.title')}</h3>
           <figure>
@@ -22,10 +20,14 @@ const Projects = () => {
             <figcaption>{translate('projects.item1.description')}</figcaption>
           </figure>
           <div className="btn-panel">
-            <a href="#" className="link button">
+            <a className="link button" onClick={closeModal1}>
               {translate('projects.item1.button1')}
             </a>
-            <a href="#" className="link button">
+            <a
+              href="https://mati-fernandez.github.io/random-quote-machine/"
+              target="_blank"
+              className="link button"
+            >
               {translate('projects.item1.button2')}
             </a>
           </div>
