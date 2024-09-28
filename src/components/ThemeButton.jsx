3c56eb286c1a472c12470cc5626d1eccc48b0189
Matias Fernandez <matifer.dev@gmail.com>
2024-09-28 13:54:00 -0300

    Browser theme detection and localStorage added

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -6,29 +6,20 @@ import Sun from '../assets/Sun';
 import Moon from '../assets/Moon';
 
 const ThemeButton = () => {
-  const { theme, setTheme, fromThemeBtn, setFromThemeBtn } =
-    useContext(ThemeContext);
+  const { theme, setTheme, setFromThemeBtn } = useContext(ThemeContext);
   const [icon, setIcon] = useState(theme === 'dark' ? <Sun /> : <Moon />);
-
-  // Remover inline styles de body al finalizar carga de App
-  const removeInlineStyles = () => {
-    const body = document.querySelector('body');
-    if (body) {
-      body.style.backgroundColor = '';
-      body.style.fontWeight = '';
-    }
-  };
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIcon(theme === 'dark' ? <Sun /> : <Moon />);
     }, 600);
 
+    localStorage.setItem('theme', theme);
+
     return () => clearTimeout(timeout);
   }, [theme]);
 
   const handleClick = () => {
-    removeInlineStyles();
     setTheme(theme === 'light' ? 'dark' : 'light');
     setFromThemeBtn(true);
   };
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,12 +1,26 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') ?? document.body.getAttribute('data-theme')
+  );
   const [fromThemeBtn, setFromThemeBtn] = useState(false);
+
+  const removeInlineStyles = () => {
+    const body = document.querySelector('body');
+    if (body) {
+      body.style.backgroundColor = '';
+      body.style.fontWeight = '';
+    }
+  };
+
+  useEffect(() => {
+    removeInlineStyles();
+  }, []);
 
   return (
     <ThemeContext.Provider
