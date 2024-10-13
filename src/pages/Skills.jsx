a0de4a491c473ed4419f93e782e418e43e9ad158
Matias Fernandez <matifer.dev@gmail.com>
2024-10-12 22:13:06 -0300

    Prisma & TS added to skills; Skills percentages updated

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -13,6 +13,8 @@ import router from '../assets/react-router.png';
 import figma from '../assets/figma.png';
 import english from '../assets/english.png';
 import nextjs from '../assets/nextjs.png';
+import ts from '../assets/ts.png';
+import prisma from '../assets/prisma.png';
 
 const Skills = ({ imagePreLoad }) => {
   const { language } = useContext(TranslationContext);
@@ -67,7 +69,7 @@ const Skills = ({ imagePreLoad }) => {
             <span className="skill-name">CSS 3</span>
           </div>
           <div className="progress-bar">
-            <div className="progress" style={{ width: '45%' }}></div>
+            <div className="progress" style={{ width: '63%' }}></div>
           </div>
         </li>
         <li>
@@ -85,7 +87,16 @@ const Skills = ({ imagePreLoad }) => {
             <span className="skill-name">JavaScript</span>
           </div>
           <div className="progress-bar">
-            <div className="progress" style={{ width: '57%' }}></div>
+            <div className="progress" style={{ width: '63%' }}></div>
+          </div>
+        </li>{' '}
+        <li>
+          <div className="skill-container">
+            <img id="ts-icon" src={ts} className="tech" alt="TypeScript" />
+            <span className="skill-name">TypeScript</span>
+          </div>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: '11%' }}></div>
           </div>
         </li>
         <li>
@@ -94,7 +105,7 @@ const Skills = ({ imagePreLoad }) => {
             <span className="skill-name">Git</span>
           </div>
           <div className="progress-bar">
-            <div className="progress" style={{ width: '27%' }}></div>
+            <div className="progress" style={{ width: '33%' }}></div>
           </div>
         </li>
         <li>
@@ -103,13 +114,22 @@ const Skills = ({ imagePreLoad }) => {
             <span className="skill-name">React JS</span>
           </div>
           <div className="progress-bar">
-            <div className="progress" style={{ width: '50%' }}></div>
+            <div className="progress" style={{ width: '60%' }}></div>
           </div>
         </li>
         <li>
           <div className="skill-container">
             <img src={nextjs} className="tech" alt="Next.js" />
             <span className="skill-name">Next.js</span>
+          </div>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: '11%' }}></div>
+          </div>
+        </li>{' '}
+        <li>
+          <div className="skill-container">
+            <img src={prisma} className="tech" alt="Prisma" />
+            <span className="skill-name">Prisma</span>
           </div>
           <div className="progress-bar">
             <div className="progress" style={{ width: '11%' }}></div>
@@ -121,7 +141,7 @@ const Skills = ({ imagePreLoad }) => {
             <span className="skill-name">Redux</span>
           </div>
           <div className="progress-bar">
-            <div className="progress" style={{ width: '20%' }}></div>
+            <div className="progress" style={{ width: '11%' }}></div>
           </div>
         </li>
         <li>
