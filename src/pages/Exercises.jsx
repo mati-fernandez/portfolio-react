f4ce0d11c80aa838a4af3b86867a7ba162bc3ed3
Matias Fernandez <matifer.dev@gmail.com>
2024-12-02 22:10:38 -0300

    Exercises page fetching mode done

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,31 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { TranslationContext } from '../context/TranslationContext';
-import imgdom from '../assets/dom-ejercicios.png';
-import imgcan from '../assets/dibujo-canvas.png';
-import imgtet from '../assets/tetris-vanilla.png';
-import imgmon from '../assets/monkeytype.png';
-import imgexc from '../assets/excel.png';
-import imgnextjsblog from '../assets/nextjs-blog.png';
 import Info from '../assets/Info';
 
-const links = [
-  'https://mati-fernandez.github.io/portfolio-vanilla-js/dom-ejercicios/index.html',
-  'https://mati-fernandez.github.io/portfolio-vanilla-js/mis-proyectos/dibujo-canvas/index.html',
-  'https://mati-fernandez.github.io/portfolio-vanilla-js/mis-proyectos/tetris-vanilla/index.html',
-  'https://mati-fernandez.github.io/portfolio-vanilla-js/mis-proyectos/monkeytype/index.html',
-  'https://mati-fernandez.github.io/portfolio-vanilla-js/mis-proyectos/excel/index.html',
-  'https://mati-nextjs-blog.vercel.app/',
-];
+const Exercises = ({ notFirstLoad, handleOpenModal }) => {
+  const { translate, getImage } = useContext(TranslationContext);
 
-const Exercises = ({ notFirstLoad, handleOpenModal, imagePreLoad }) => {
-  const { translate } = useContext(TranslationContext);
+  const imagesData = getImage('exercises');
+  console.log('imagesData:', imagesData);
 
-  useEffect(() => {
-    const urls = [imgdom, imgcan, imgtet, imgmon];
-    imagePreLoad(urls);
-  }, []);
+  const translationsData = translate('exercises.exercisesList');
+  console.log('translationData:', translationsData);
 
   return (
     <div className="page">
@@ -34,62 +20,20 @@ const Exercises = ({ notFirstLoad, handleOpenModal, imagePreLoad }) => {
         handleOpenModal={handleOpenModal}
         itemKey={'exercises.info'}
       />
-      <button
-        className="long-text button link"
-        onClick={() =>
-          handleOpenModal('exercises.exercisesList.dom', imgdom, links[0])
-        }
-      >
-        {translate('exercises.exercisesList.dom.title')}
-      </button>
-      <button
-        className="long-text button link"
-        onClick={() =>
-          handleOpenModal('exercises.exercisesList.draw', imgcan, links[1])
-        }
-      >
-        {translate('exercises.exercisesList.draw.title')}
-      </button>
-      <button
-        className="long-text button link"
-        onClick={() =>
-          handleOpenModal('exercises.exercisesList.tetris', imgtet, links[2])
-        }
-      >
-        {translate('exercises.exercisesList.tetris.title')}
-      </button>
-      <button
-        className="long-text button link"
-        onClick={() =>
-          handleOpenModal(
-            'exercises.exercisesList.monkeytype',
-            imgmon,
-            links[3]
-          )
-        }
-      >
-        {translate('exercises.exercisesList.monkeytype.title')}
-      </button>
-      <button
-        className="long-text button link"
-        onClick={() =>
-          handleOpenModal('exercises.exercisesList.excel', imgexc, links[4])
-        }
-      >
-        {translate('exercises.exercisesList.excel.title')}
-      </button>
-      <button
-        className="long-text button link"
-        onClick={() =>
-          handleOpenModal(
-            'exercises.exercisesList.nextjs-blog',
-            imgnextjsblog,
-            links[5]
-          )
-        }
-      >
-        {translate('exercises.exercisesList.nextjs-blog.title')}
-      </button>
+      {Object.keys(translationsData).map((key) => (
+        <button
+          key={key}
+          className="long-text button link"
+          onClick={() =>
+            handleOpenModal(
+              `exercises.exercisesList.` + key,
+              `exercises.` + key
+            )
+          }
+        >
+          {translationsData[key].title}
+        </button>
+      ))}
     </div>
   );
 };
