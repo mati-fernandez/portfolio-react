89a779c8181b6a2333893385bd1a006ba7540d8d
Matias Fernandez <matifer.dev@gmail.com>
2025-01-24 20:58:51 -0300

    Vite configuration updated to support deployment on Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-react/',
+  base: process.env.VERCEL === '1' ? '/' : '/portfolio-react/',
 });
