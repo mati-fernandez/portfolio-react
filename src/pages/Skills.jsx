04f35b28e8d35ca483a26ea4a1944de89ae1e10d
Matias Fernandez <matifer.dev@gmail.com>
2025-04-17 22:20:18 -0300

    "Alt" propery construction changed for skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,24 +3,9 @@ import { useContext, useState } from 'react';
 import { TranslationContext } from '../context/contexts';
 import Loader from '../components/Loader';
 
-const Skills = () => {
-  const { getImage } = useContext(TranslationContext);
-  const data = getImage('skills');
-
-  return (
-    <div className="flex-grow overflow-y-hidden page" style={{ padding: '0' }}>
-      <ul className="skills-list">
-        {Object.keys(data).map((skill) => (
-          <SkillItem key={skill} skillData={data[skill]} />
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const SkillItem = ({ skillData }) => {
+const SkillItem = ({ skillKey, skillData }) => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const { endpoint, contentDevPath, contentBuildPath } =
+  const { translate, endpoint, contentDevPath, contentBuildPath } =
     useContext(TranslationContext);
   const srcPath =
     endpoint === 'build' ? `${contentBuildPath}` : `${contentDevPath}`;
@@ -30,17 +15,6 @@ const SkillItem = ({ skillData }) => {
       <div className="skill-container">
         {/* Loader se muestra mientras la imagen no está cargada */}
         <div style={{ position: 'relative', marginRight: '.5rem' }}>
-          {/* <div
-            className="loader"
-            style={{
-              position: 'absolute',
-              width: '1rem', // El mismo tamaño que la imagen
-              height: '1rem',
-              borderWidth: '.1rem',
-              aspectRatio: 1,
-              display: isLoaded ? 'none' : 'block', // Se oculta al cargar la imagen
-            }}
-          ></div> */}
           {!isLoaded && (
             <Loader
               size="1rem"
@@ -53,19 +27,36 @@ const SkillItem = ({ skillData }) => {
         {/* Imagen que se muestra al cargar */}
         <img
           src={`${srcPath}${skillData.src}`}
-          alt={skillData.alt}
+          alt={translate(`skills.${skillKey}.title`)}
           className="tech"
           style={{
             visibility: isLoaded ? 'visible' : 'hidden', // Oculta visualmente la imagen hasta que cargue
           }}
           onLoad={() => setIsLoaded(true)}
         />
-        <span className="skill-name">{skillData.alt}</span>
+        <span className="skill-name">
+          {translate(`skills.${skillKey}.title`)}
+        </span>
       </div>
       <div className="progress-bar">
         <div className="progress" style={{ width: skillData.progress }}></div>
       </div>
     </li>
+  );
+};
+
+const Skills = () => {
+  const { getImage } = useContext(TranslationContext);
+  const data = getImage('skills');
+
+  return (
+    <div className="flex-grow overflow-y-hidden page" style={{ padding: '0' }}>
+      <ul className="skills-list">
+        {Object.keys(data).map((skill) => (
+          <SkillItem key={skill} skillData={data[skill]} skillKey={skill} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
