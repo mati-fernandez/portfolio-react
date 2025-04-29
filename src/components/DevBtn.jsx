eef8b57f7fee48b3902c3e442de5a6028c11ec58
Matias Fernandez <matifer.dev@gmail.com>
2025-04-29 20:23:25 -0300

    DevBtn migrated

--- src/components/DevBtn.jsx
+++ src/components/DevBtn.jsx
@@ -1,33 +1,27 @@
-import { useContext } from 'react';
-import { TranslationContext } from '../context/contexts';
+import { useContext } from "react";
+import { TranslationContext } from "../context/contexts";
 
 const DevBtn = () => {
   const { endpoint, setEndpoint } = useContext(TranslationContext);
 
   const handleClick = () => {
-    setEndpoint((prev) => (prev === 'build' ? 'dev' : 'build'));
+    setEndpoint((prev) => (prev === "build" ? "dev" : "build"));
   };
 
+  const baseClasses =
+    "border-primary hover:bg-secondary-alpha absolute top-[2.5vh] left-[2vh] cursor-pointer rounded-4xl border-1 border-solid p-[0.5rem] opacity-[0.7] landscape:top-[12vh] landscape:left-[2vh] landscape:text-[0.5rem]";
+
   return (
-    <>
-      {endpoint === 'build' ? (
-        <button id="dev-mode-btn" onClick={handleClick}>
-          Build <br /> Endpoint
-        </button>
-      ) : (
-        <button
-          id="dev-mode-btn"
-          style={{
-            backgroundColor: 'red',
-            color: 'black',
-            fontWeight: 'bolder',
-          }}
-          onClick={handleClick}
-        >
-          Dev <br /> Endpoint
-        </button>
-      )}
-    </>
+    <button
+      className={`${
+        endpoint === "build"
+          ? `${baseClasses}`
+          : `${baseClasses} bg-[red] font-extrabold text-[black]`
+      }`}
+      onClick={handleClick}
+    >
+      {endpoint === "build" ? "Build" : "Dev"} <br /> Endpoint
+    </button>
   );
 };
 
