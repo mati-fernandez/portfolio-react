9b0fd8d0872aa0432b48f92468527321ba716f6f
Matias Fernandez <matifer.dev@gmail.com>
2025-05-15 16:57:21 -0300

    💍 ring ring... it's the burger button glowing 😏

--- src/components/MobileFooterWrapper.jsx
+++ src/components/MobileFooterWrapper.jsx
@@ -11,11 +11,6 @@ const MobileFooterWrapper = ({ setFromMenuBtn }) => {
   const { translate } = useContext(TranslationContext);
   const { showMenu, setShowMenu } = useContext(PageContext);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-    showMenu ? setFromMenuBtn(true) : setFromMenuBtn(false);
-  };
-
   return (
     <div className="pointer-events-none absolute inset-0 h-[100svh] overflow-hidden landscape:hidden">
       {showMenu && (
@@ -23,11 +18,9 @@ const MobileFooterWrapper = ({ setFromMenuBtn }) => {
       )}
       <footer className="bg-background-alpha pointer-events-auto absolute bottom-0 flex h-[10vh] w-screen items-center justify-between px-[4vw] py-0">
         <Link className="link" to="/" onClick={() => setShowMenu(false)}>
-          <p className="m-0 p-[2vw] text-[1.8rem] font-normal">Matias</p>
+          <p className="m-0 ml-1 p-[2vw] text-[1.8rem] font-normal">Matias</p>
         </Link>
-        <a className="cursor-pointer p-[2vh]" onClick={toggleMenu}>
-          <Hamburguer showMenu={showMenu} />
-        </a>
+        <Hamburguer setFromMenuBtn={setFromMenuBtn} />
       </footer>
     </div>
   );
